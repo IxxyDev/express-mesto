b190c8acc0e0975d4bdf5df7a20ac7c0e1b03c54
IxxyDev <denisenkoforwork@gmail.com>
2020-08-28 18:51:16 +0300

    fix: some more linter fixes

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const { PORT = 3000 } = process.env;
-const cardsRouter  = require('./routes/cards');
+const cardsRouter = require('./routes/cards');
 const usersRouter = require('./routes/users');
 const notFoundRouter = require('./routes/404notFound');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/cards',cardsRouter);
+app.use('/cards', cardsRouter);
 app.use('/users', usersRouter);
 app.use('/*', notFoundRouter);
 
-
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`);
-})
+  console.log(`Listening on ${PORT}`);
+});
--- controllers/users.js
+++ controllers/users.js
@@ -1,34 +1,33 @@
-const getFile = require('../utils/getFile');
 const path = require('path');
+const getFile = require('../utils/getFile');
 
 const getUsers = (req, res) => getFile(path.join(__dirname, '../data/users.json'))
-  .then(users => {
+  .then((users) => {
     res
-    .status(200)
-    .send(JSON.parse(users))
-  }).catch(err => {
+      .status(200)
+      .send(JSON.parse(users));
+  }).catch((err) => {
     res
       .status(500)
-      .send({ message: `Упс, ошибочка ${err}` })
+      .send({ message: `Упс, ошибочка ${err}` });
   });
 
-  const getUser = (req, res) => getFile(path.join(__dirname, '../data/users.json'))
-    .then(user => {
-      const currentUser = JSON.parse(user.find(item => item._id === req.params.id));
-      console.log(currentUser);
-      if (currentUser) {
-        return res
-          .status(200)
-          .send(currentUser);
-      }
-        res
-          .status(404)
-          .send({ message: 'Такого пользователя нет'});
-    })
-    .catch(err => {
-      res
+const getUser = (req, res) => getFile(path.join(__dirname, '../data/users.json'))
+  .then((user) => {
+    const currentUser = JSON.parse(user.find((item) => item._id === req.params.id));
+    if (currentUser) {
+      return res
+        .status(200)
+        .send(currentUser);
+    }
+    return res
+      .status(404)
+      .send({ message: 'Такого пользователя нет' });
+  })
+  .catch((err) => {
+    res
       .status(500)
-      .send({ message: `Упс, ошибочка ${err}` })
-    })
+      .send({ message: `Упс, ошибочка ${err}` });
+  });
 
-  module.exports = { getUsers, getUser };
+module.exports = { getUsers, getUser };
