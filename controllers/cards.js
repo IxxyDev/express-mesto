97d773d8df6110d02d684438699b384e402d4936
IxxyDev <denisenko@uchi.ru>
2020-09-19 16:29:49 +0300

    feat: add like\dislike to card

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,4 +1,5 @@
 const { Card } = require('../models/card');
+const { createBadRequestErr, createNotFoundErr } = require('../helpers/errors');
 
 const getCards = (req, res, next) => {
   Card.find({})
@@ -10,17 +11,48 @@ const getCards = (req, res, next) => {
 const createCard = (req, res, next) => {
   const { name, link } = req.body;
 
-  Card.create({ name, link, owner: req.user._id})
-  .then((card) => res.send { data: card })
-  .catch((error) => createError )
-  .catch(next);
-}
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => res.send({ data: card }))
+    .catch((error) => createBadRequestErr(error, error.message.INCORRECT_CARD_DATA))
+    .catch(next);
+};
 
 const deleteCard = (req, res, next) => {
   const { cardId } = req.params;
-  
-  Card.findByIdAndRemove(cardId)
-  .orFail(createCustomError)
-}
 
-module.exports = { getCards };
+  Card.findByIdAndRemove(cardId)
+    .orFail()
+    .then((card) => res.send({ data: card }))
+    .catch((error) => error.message.CARD_NOT_FOUND)
+    .catch(next);
+};
+
+const likeCard = (req, res, next) => {
+  Card.findByIdAndUpdate(
+    req.params._id,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  ).orFail()
+    .then((likes) => res.send({ data: likes }))
+    .catch((error) => createNotFoundErr(error, error.message.CARD_NOT_FOUND))
+    .catch(next);
+};
+
+const unlikeCard = (req, res, next) => {
+  Card.findByIdAndUpdate(
+    req.params._id,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  ).orFail()
+    .then((likes) => res.send({ data: likes }))
+    .catch((error) => createNotFoundErr(error, error.message.CARD_NOT_FOUND))
+    .catch(next);
+};
+
+module.exports = {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  unlikeCard,
+};
