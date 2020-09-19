0d15a272c614e369819d3a364f92c8f05ecfb38b
IxxyDev <denisenko@uchi.ru>
2020-09-19 16:45:13 +0300

    feat: add routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const { getCards } = require('../controllers/cards.js');
+const {
+  getCards, createCard, likeCard, deleteCard, unlikeCard,
+} = require('../controllers/cards.js');
 
 const cardsRouter = express.Router();
 cardsRouter.get('/', getCards);
+cardsRouter.post('/', createCard);
+cardsRouter.delete('/:id', deleteCard);
+
+cardsRouter.delete('/:id/likes', unlikeCard);
+cardsRouter.put('/:_id/liles/', likeCard);
 
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { getUsers, getUser, createUser } = require('../controllers/users.js');
+const {
+  getUsers, getUser, createUser, updateUser, updateUserAvatar,
+} = require('../controllers/users.js');
 
 const usersRouter = express.Router();
 usersRouter.get('/', getUsers);
 usersRouter.get('/:id', getUser);
 usersRouter.post('/users', createUser);
+usersRouter.patch('/me', updateUser);
+usersRouter.patch('/me/avatar', updateUserAvatar);
 
 module.exports = usersRouter;
