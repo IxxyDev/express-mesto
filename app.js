85d271d40f0d8c1dfa7e758e03a530788761ae5b
IxxyDev <denisenko@uchi.ru>
2021-01-25 20:16:56 +0300

    fix: change port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const { PORT = 3000 } = process.env;
+const { PORT = 3005 } = process.env;
 const { ERROR_CODE, ERROR_MESSAGE } = require('./utils/constants');
 const cardsRouter = require('./routes/cards');
 const usersRouter = require('./routes/users');
