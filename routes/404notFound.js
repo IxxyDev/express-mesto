dd63db2fe7b86cf23000b7017d13816d1c8f7882
IxxyDev <denisenko@uchi.ru>
2021-01-28 11:36:42 +0300

    fix: minor fixes

--- routes/404notFound.js
+++
@@ -1,9 +0,0 @@
-const notFoundRouter = require('express').Router();
-
-notFoundRouter.all('/', (req, res) => {
-  res
-    .status(404)
-    .send({ message: 'Запрашиваемый ресурс не найден' });
-});
-
-module.exports = notFoundRouter;
