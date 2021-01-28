7f88ff870b3a3c3924a809b25aef6d459aaaf3e5
IxxyDev <denisenko@uchi.ru>
2021-01-28 11:20:56 +0300

    feat: validate routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,14 +1,41 @@
 const express = require('express');
+const { celebrate, Joi } = require('celebrate');
+const { isURL } = require('validator');
 const {
   getCards, createCard, likeCard, deleteCard, unlikeCard,
 } = require('../controllers/cards.js');
 
 const cardsRouter = express.Router();
 cardsRouter.get('/', getCards);
-cardsRouter.post('/', createCard);
-cardsRouter.delete('/:cardId', deleteCard);
 
-cardsRouter.delete('/:cardId/likes', unlikeCard);
-cardsRouter.put('/:cardId/likes', likeCard);
+cardsRouter.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().custom((url, helpers) => {
+      if (!isURL(url)) {
+        return helpers.error('Ссылка невалидна');
+      }
+      return url;
+    }),
+  }),
+}), createCard);
+
+cardsRouter.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), deleteCard);
+
+cardsRouter.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), unlikeCard);
+
+cardsRouter.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), likeCard);
 
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { celebrate, Joi } = require('celebrate');
+const { isURL } = require('validator');
 const {
   getUsers, getMe, getUserById, updateUser, updateUserAvatar,
 } = require('../controllers/users.js');
@@ -6,8 +8,28 @@ const {
 const usersRouter = express.Router();
 usersRouter.get('/', getUsers);
 usersRouter.get('/me', getMe);
-usersRouter.get('/:_id', getUserById);
-usersRouter.patch('/me', updateUser);
-usersRouter.patch('/me/avatar', updateUserAvatar);
+usersRouter.get('/:_id', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().hex().length(24),
+  }),
+}), getUserById);
+
+usersRouter.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUser);
+
+usersRouter.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().custom((url, helpers) => {
+      if (!isURL(url)) {
+        return helpers.error('Ссылка невалидна');
+      }
+      return url;
+    }),
+  }),
+}), updateUserAvatar);
 
 module.exports = usersRouter;
